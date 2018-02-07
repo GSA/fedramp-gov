a8d2ed1ab358ed7c212e6d209a3f6ec4e79b8503
Scott Weber <stephen.weber@gsa.gov>
2018-02-07 14:57:13 -0500

    Retuning results from the RSS feed only on the results page ATM

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,25 @@
     });
   }
 })(document, window, $);
+
+
+// searchgov
+$("#search-button").click(search);
+
+$('#results').empty();
+
+    function search(){
+      var searchTerm = $("#searchgovinput").val();
+      var url = 'https://search.usa.gov/api/v2/search?affiliate=fedramp&access_key=pkwSg7Bh0i05jHA-ubLZkuVx5gz_AIL0HVPc09Vq_VQ=&query=' + searchTerm;
+
+      $('#results').empty();
+
+      $.getJSON(url, function(json) {
+        console.log("GOT SOME DATA:", json.web.results)
+
+        json.web.results.forEach(function(result){
+          console.log(result)
+          $('#results').append("<li><a href='" + result.url + "'>" + result.title + "</a></li>")
+        });
+      });
+    };
