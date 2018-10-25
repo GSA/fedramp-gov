4cb64bb145c4d85d32e22a5085109dd362fa3eee
cbukovac <claire.bukovac@gsa.gov>
2018-10-25 14:29:25 -0400

    posting new blog

---
+++ _posts/2018-10-25-fedramp-connect-business-cases-due-november-9th.m
@@ -0,0 +1,22 @@
+---
+title: FedRAMP Connect Business Cases Due November 9th 
+permalink: /fedramp-connect-business-cases-due-november-9th/
+body-class: page-blog
+image: /assets/img/blog-images/Blog_FedRAMP_Connect.png
+author: FedRAMP
+layout: blog-page
+---
+With the due date for FedRAMP Connect Business Cases quickly approaching on November 9th, we want to remind CSPs of the resources available to help you through the JAB prioritization process. 
+
+As you may remember, FedRAMP Connect is part of the first step in pursuing a <a href="https://www.fedramp.gov/jab-authorization/">Provisional Authority to Operate (P-ATO) from the Joint Authorization Board (JAB)</a>. The FedRAMP PMO aims to prioritize three CSPs each quarter and evaluates Business Cases on a rolling basis in order to determine which CSPs will work with the JAB. 
+
+Our <a href="{{site.baseurl}}/assets/resources/documents/CSP_JAB_P-ATO_Prioritization_Criteria_and_Guidance.pdf">JAB Prioritization Criteria and Guidance</a> document provides an overview of the FedRAMP Connect process, the necessary attachments for the Business Case submission, and instructions on how to successfully participate in FedRAMP Connect. If you are interested in being prioritized to work with the JAB, please read this document in its entirety and review the required Business Case attachments before getting started. This document includes information on: 
+
+ * Adobe Systems, Inc. The JAB prioritization criteria
+ * Lookout, Inc. How to submit a Business Case
+ * Smartsheet, Inc. What qualifies as current, indirect, and potential demand
+ * Zscaler, Inc.The Business Case evaluation methodology
+
+If you have questions about FedRAMP Connect, would like advice on your Business Case, or need help qualifying their demand, feel free to reach out to info@fedramp.gov to set up a one-on-one coaching call with the PMO. 
+
+**We look forward to reviewing your Business Case submission, due November 9th by 5:00pm eastern to info@fedramp.gov!**
