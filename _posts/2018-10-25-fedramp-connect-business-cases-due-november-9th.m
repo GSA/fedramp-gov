7dff5f5a20df13253e29442955cac08fcd3845e9
cbukovac <claire.bukovac@gsa.gov>
2018-10-25 16:07:42 -0400

    updated formatting

--- _posts/2018-10-25-fedramp-connect-business-cases-due-november-9th.m
+++ _posts/2018-10-25-fedramp-connect-business-cases-due-november-9th.m
@@ -6,17 +6,17 @@ image: /assets/img/blog-images/Blog_FedRAMP_Connect.png
 author: FedRAMP
 layout: blog-page
 ---
-With the due date for FedRAMP Connect Business Cases quickly approaching on November 9th, we want to remind CSPs of the resources available to help you through the JAB prioritization process. 
+<p>With the due date for FedRAMP Connect Business Cases quickly approaching on November 9th, we want to remind CSPs of the resources available to help you through the JAB prioritization process.</p>
 
-As you may remember, FedRAMP Connect is part of the first step in pursuing a <a href="https://www.fedramp.gov/jab-authorization/">Provisional Authority to Operate (P-ATO) from the Joint Authorization Board (JAB)</a>. The FedRAMP PMO aims to prioritize three CSPs each quarter and evaluates Business Cases on a rolling basis in order to determine which CSPs will work with the JAB. 
+<p>As you may remember, FedRAMP Connect is part of the first step in pursuing a <a href="https://www.fedramp.gov/jab-authorization/">Provisional Authority to Operate (P-ATO) from the Joint Authorization Board (JAB)</a>. The FedRAMP PMO aims to prioritize three CSPs each quarter and evaluates Business Cases on a rolling basis in order to determine which CSPs will work with the JAB.</p>
 
-Our <a href="{{site.baseurl}}/assets/resources/documents/CSP_JAB_P-ATO_Prioritization_Criteria_and_Guidance.pdf">JAB Prioritization Criteria and Guidance</a> document provides an overview of the FedRAMP Connect process, the necessary attachments for the Business Case submission, and instructions on how to successfully participate in FedRAMP Connect. If you are interested in being prioritized to work with the JAB, please read this document in its entirety and review the required Business Case attachments before getting started. This document includes information on: 
+<p>Our <a href="{{site.baseurl}}/assets/resources/documents/CSP_JAB_P-ATO_Prioritization_Criteria_and_Guidance.pdf">JAB Prioritization Criteria and Guidance</a> document provides an overview of the FedRAMP Connect process, the necessary attachments for the Business Case submission, and instructions on how to successfully participate in FedRAMP Connect. If you are interested in being prioritized to work with the JAB, please read this document in its entirety and review the required Business Case attachments before getting started. This document includes information on:</p> 
 
- * Adobe Systems, Inc. The JAB prioritization criteria
- * Lookout, Inc. How to submit a Business Case
- * Smartsheet, Inc. What qualifies as current, indirect, and potential demand
- * Zscaler, Inc.The Business Case evaluation methodology
+ * The JAB prioritization criteria
+ * How to submit a Business Case
+ * What qualifies as current, indirect, and potential demand
+ * The Business Case evaluation methodology
 
-If you have questions about FedRAMP Connect, would like advice on your Business Case, or need help qualifying their demand, feel free to reach out to info@fedramp.gov to set up a one-on-one coaching call with the PMO. 
+<p>If you have questions about FedRAMP Connect, would like advice on your Business Case, or need help qualifying their demand, feel free to reach out to info@fedramp.gov to set up a one-on-one coaching call with the PMO.</p>
 
-**We look forward to reviewing your Business Case submission, due November 9th by 5:00pm eastern to info@fedramp.gov!**
+<p>**We look forward to reviewing your Business Case submission, due November 9th by 5:00pm eastern to info@fedramp.gov!**</p>
