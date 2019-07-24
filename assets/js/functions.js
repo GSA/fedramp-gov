df6d7c079e38aa5f560ed41f83535c77be8e4b2f
Lance <lance.smith@echoorigin.com>
2019-07-23 22:47:43 -0400

    nav updates using css for full compliance

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,1 +1,7 @@
 $('ul.sub-sub-menu').parent('li').addClass("parent");
+
+$('ul.usa-nav-primary li a').keypress(function(event) {
+    if (event.which === 32 || event.which === 13) {
+    $(":focus").click();	
+    }
+});
