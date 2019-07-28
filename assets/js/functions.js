b1fa6a4198611d13230ae2ad6ac5432fc92cefec
Lance <lance.smith@echoorigin.com>
2019-07-28 15:09:28 -0400

    added focus-within polyfill

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,3 +5,36 @@ $('ul.usa-nav-primary li a').keypress(function(event) {
     $(":focus").click();	
     }
 });
+
+
+(function(window, document){
+	'use strict';
+	var slice = [].slice;
+	var removeClass = function(elem){
+		elem.classList.remove('focus-within');
+	};
+	var update = (function(){
+		var running, last;
+		var action = function(){
+			var element = document.activeElement;
+			running = false;
+			if(last !== element){
+				last = element;
+				slice.call(document.getElementsByClassName('focus-within')).forEach(removeClass);
+				while(element && element.classList){
+					element.classList.add('focus-within');
+					element = element.parentNode;
+				}
+			}
+		};
+		return function(){
+			if(!running){
+				requestAnimationFrame(action);
+				running = true;
+			}
+		};
+	})();
+	document.addEventListener('focus', update, true);
+	document.addEventListener('blur', update, true);
+	update();
+})(window, document);
