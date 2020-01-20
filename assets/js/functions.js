4f971ab806f168bedfeba0e6996441995f5f99f3
Lance <lance.smith@echoorigin.com>
2020-01-20 15:02:23 -0500

    updated function for new nav

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,44 @@
 $('ul.sub-sub-menu').parent('li').addClass("parent");
 
+$('ul.usa-nav-primary li a').keypress(function(event) {
+    if (event.which === 32 || event.which === 13) {
+    $(":focus").click();	
+    }
+});
+
+
+(function(window, document){
+	'use strict';
+	var slice = [].slice;
+	var removeClass = function(elem){
+		elem.classList.remove('focus-within');
+	};
+	var update = (function(){
+		var running, last;
+		var action = function(){
+			var element = document.activeElement;
+			running = false;
+			if(last !== element){
+				last = element;
+				slice.call(document.getElementsByClassName('focus-within')).forEach(removeClass);
+				while(element && element.classList){
+					element.classList.add('focus-within');
+					element = element.parentNode;
+				}
+			}
+		};
+		return function(){
+			if(!running){
+				requestAnimationFrame(action);
+				running = true;
+			}
+		};
+	})();
+	document.addEventListener('focus', update, true);
+	document.addEventListener('blur', update, true);
+	update();
+})(window, document);
+
 window.onload = function () {
     document.getElementById('button').onclick = function () {
         document.getElementById('modal').style.display = "none"
