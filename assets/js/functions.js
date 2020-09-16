a3291a4a7f114e58223ab36429827a8f2d61dadb
Michael Frising <mike.frising@echooorigin.com>
2020-09-16 15:57:43 -0400

    Council adjustments

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,22 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropDown() {
+  document.getElementsByClassName("myDropdown").classList.toggle("show");
+}
 
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
 function checkFilter(inId) {
   $(inId).click();
