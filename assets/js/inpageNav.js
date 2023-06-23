69916e4364b899aa2c1f2b25c9c32694f04f99ba
michaelfrising <michael.frising@gsa.gov>
2023-06-23 10:47:33 -0400

    training update

--- assets/js/inpageNav.js
+++ assets/js/inpageNav.js
@@ -43,3 +43,51 @@ $(window).scroll(function(){
          .end().filter("[href='#"+id+"']").parent().addClass("active");
    }                   
 });
+
+
+
+// // Cache selectors
+// var lastIdHoriz,
+//     topMenu = $("#horizontal-top-menu"),
+//     topMenuHeight = topMenu.outerHeight()+80,
+//     // All list items
+//     menuItems = topMenu.find("a"),
+//     // Anchors corresponding to menu items
+//     scrollItems = menuItems.map(function(){
+//       var item = $($(this).attr("href"));
+//       if (item.length) { return item; }
+//     });
+
+// // Bind click handler to menu items
+// // so we can get a fancy scroll animation
+// menuItems.click(function(e){
+//   var href = $(this).attr("href"),
+//       offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+//   $('html, body').stop().animate({ 
+//       scrollTop: offsetTop
+//   }, 300);
+//   e.preventDefault();
+// });
+
+// // Bind to scroll
+// $(window).scroll(function(){
+//    // Get container scroll position
+//    var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+//    // Get id of current scroll item
+//    var cur = scrollItems.map(function(){
+//      if ($(this).offset().top < fromTop)
+//        return this;
+//    });
+//    // Get the id of the current element
+//    cur = cur[cur.length-1];
+//    var id = cur && cur.length ? cur[0].id : "";
+   
+//    if (lastIdHoriz !== id) {
+//        lastIdHoriz = id;
+//        // Set/remove active class
+//        menuItems
+//          .parent().removeClass("active")
+//          .end().filter("[href='#"+id+"']").parent().addClass("active");
+//    }                   
+// });
